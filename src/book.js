becb75263b292fa0ef7a3f7170d0aab7390f28b6
Renni9807 <practiceID98@gmail.com>
2024-07-24 20:44:24 +0800

    final ver

--- src/book.js
+++ src/book.js
@@ -5,7 +5,6 @@ const Book = (props) => {
       <span className="book-number">#{id}</span>
       <img src={img} alt="book"></img>
       <h2>{title}</h2>
-      <button onClick={() => console.log(title)}>display title</button>
       <h4>{author}</h4>
     </article>
   );
